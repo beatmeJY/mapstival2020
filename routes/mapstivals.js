9db1fed5fcd4bbb768693ecd71a6392b15115784
Choi JiYoul <wldbf97@naver.com>
2020-09-10 18:59:11 +0900

    code edit

--- routes/mapstivals.js
+++ routes/mapstivals.js
@@ -1,35 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const models = require("../models");
-const board = models.Board;
-const sequelize = require("sequelize");
-const Op = sequelize.Op;
 const API = require("../config/apikey");
 const axios = require("axios");
-const GAPI = require("../config/gapikey.json");
-const naverid = require("../config/naverKey.json");
-const naversecret = require("../config/naverKey.json");
+const db = require("../models");
 
 // API key 선언
-const client_id = naverid.client_id;
-const client_secret = naversecret.client_secret;
-const GAPI_KEY = GAPI.GAPI_KEY;
-const API_KEY = API.API_KEY;
-const API_URL = API.API_URL;
-const API_ETC = API.API_ETC;
-
-let INFO_URL = null;
-let URL = null;
-let tourData = [];
-let FNumber = 6;
-let hidden = "";
-
-// 맵 초기값 셋팅
-let startYear = 2020;
-let startMonth = 05;
-let startDate = 20200501;
-let f_area = 1;
-let selected = [];
+const GOOGLE_KEY = API.googleAPI.API_KEY;
+const NAVER_ID = API.naverAPI.CLIENT_ID;
+const NAVER_SECRET = API.naverAPI.CLIENT_SECRET;
+const FESTIVAL_KEY = API.festivalAPI.API_KEY;
+const FESTIVAL_URL = API.festivalAPI.API_URL;
+const FESTIVAL_ETC = API.festivalAPI.API_ETC;
 
 let reference = null;
 let rating = null;
@@ -38,144 +19,56 @@ let gmapx = null;
 let gmapy = null;
 let gtitle = null;
 
-//축제 메인페이지
+//축제 메인페이지 이동 시 초기 값 전송
 router.get("/", function (req, res, next) {
-  f_area = req.query.f_area;
-
-  const newDate = new Date();
-  let Year = newDate.getFullYear();
-  let Month = newDate.getMonth();
-  //파라미타 값이 있으면 그걸로
-  if (req.query.year != null) {
-    Year = req.query.year;
-  }
-  if (req.query.Month != null) {
-    Month = req.query.Month;
-  }
-
-  if (Month < 10) {
-    Month = `0${Month}`;
-  }
-  const today = `${Year}${Month}01`;
+  // 지역 선택 초기 값
+  let area = req.query.area;
+  let newDate = new Date();
+  let year = newDate.getFullYear();
+  let month = newDate.getMonth() + 1;
 
   const selected = {
-    Month: `${Month}`,
-    Year: `${Year}`,
-    f_area: `${f_area}`,
+    month: `${month}`,
+    year: `${year}`,
+    area: `${area}`,
   };
-
-  axios
-    .get(`${apiSetting(today, f_area, FNumber)}`)
-    .then((response) => {
-      let tourData = [];
-      const list = response.data.response.body.items.item;
-      if (Array.isArray(list)) {
-        if (list != undefined) {
-          for (data in list) {
-            tourData.push(list[data]);
-          }
-        } else {
-          tourData = null;
-        }
-      } else if (response.data.response.body.items === "") {
-        tourData = null;
-      } else {
-        tourData.push(list);
-      }
-      if (tourData.length < FNumber) {
-        hidden = "ok";
-      }
-      res.render("mapstival/main", {
-        data: tourData,
-        selected: selected,
-        hidden: hidden,
-      });
-    })
-    .catch((e) => {
-      res.send(e);
-    });
+  res.render("mapstival/main", {
+    selected,
+  });
 });
 
-//축제 날짜 지역 post로 받아오기
-router.post("/main", function (req, res, next) {
-  if (req.body.startYear) {
-    startYear = req.body.startYear;
-    startMonth = req.body.startMonth;
-    startDate = startYear + startMonth + "01";
-    f_area = req.body.f_area;
-    selected = {
-      Month: `${startMonth}`,
-      Year: `${startYear}`,
-      f_area: `${f_area}`,
-    };
-    FNumber = 6;
-    hidden = "";
-  } else if (req.body.buttonClick) {
-    MaxNumber = 6 + FNumber;
-    FNumber += 6;
+//축제 API 검색 POST
+router.post("/search", async function (req, res, next) {
+  try {
+    if (req.body) {
+      const area = req.body.area,
+        pageNum = req.body.pageNum,
+        date = req.body.date;
+
+      let festivalData = await axios.get(
+        `${FESTIVAL_URL}searchFestival?serviceKey=${FESTIVAL_KEY}${FESTIVAL_ETC}&listYN=Y&areaCode=${area}&pageNo=${pageNum}&numOfRows=6&eventEndDate=${date}`
+      );
+      const data = festivalData.data.response.body;
+      res.json(data);
+    }
+  } catch (err) {
+    console.log(err);
   }
-
-  // console.log(startDate);
-
-  axios
-    .get(`${apiSetting(startDate, f_area, FNumber)}`)
-    .then((response) => {
-      tourData = [];
-      const list = response.data.response.body.items.item;
-      if (Array.isArray(list)) {
-        if (list != undefined) {
-          for (data in list) {
-            tourData.push(list[data]);
-          }
-        } else {
-          console.log("두번째 if");
-          tourData = null;
-        }
-      } else if (response.data.response.body.items == "") {
-        console.log("첫번째 if");
-        tourData = "";
-      } else {
-        tourData.push(list);
-      }
-      if (tourData.length < FNumber) {
-        hidden = "ok";
-      }
-      res.render("mapstival/main", {
-        data: tourData,
-        selected: selected,
-        hidden: hidden,
-      });
-    })
-    .catch((e) => {
-      res.send(e);
-    });
 });
 
 //상세정보 페이지 이동
-//비동기 처리방식
+//동기 처리방식
 router.get("/detail", async function (req, res, next) {
-  let setting = req.body;
-  let value = "";
+  let value = req.query.id;
 
-  value = req.query.id;
-
-  INFO_URL = `${API_URL}detailCommon?ServiceKey=${API_KEY}&contentId=${value}${API_ETC}&defaultYN=Y&firstImageYN=Y&addrinfoYN=Y&overviewYN=Y&mapinfoYN=Y`;
-  let DETAIL_URL = `${API_URL}detailIntro?ServiceKey=${API_KEY}${API_ETC}&contentId=${value}&contentTypeId=15`;
+  INFO_URL = `${FESTIVAL_URL}detailCommon?ServiceKey=${FESTIVAL_KEY}&contentId=${value}${FESTIVAL_ETC}&defaultYN=Y&firstImageYN=Y&addrinfoYN=Y&overviewYN=Y&mapinfoYN=Y`;
+  let DETAIL_URL = `${FESTIVAL_URL}detailIntro?ServiceKey=${FESTIVAL_KEY}${FESTIVAL_ETC}&contentId=${value}&contentTypeId=15`;
   let tourData = null;
   let detail_Data = null;
 
   try {
     const INFO = await axios.get(INFO_URL);
     const DETAIL = await axios.get(DETAIL_URL);
-
-    //db 데이터 가져오기
-    const dbData = await board.findAll({
-      where: {
-        festival_id: {
-          [Op.like]: value,
-        },
-      },
-    });
 
     tourData = INFO.data.response.body.items.item;
     detail_Data = DETAIL.data.response.body.items.item;
@@ -191,8 +84,8 @@ router.get("/detail", async function (req, res, next) {
       "&display=6"; // json 결과
     var config = {
       headers: {
-        "X-Naver-Client-Id": client_id,
-        "X-Naver-Client-Secret": client_secret,
+        "X-Naver-Client-Id": NAVER_ID,
+        "X-Naver-Client-Secret": NAVER_SECRET,
       },
     };
     try {
@@ -203,8 +96,8 @@ router.get("/detail", async function (req, res, next) {
     }
     //구글 평점 리뷰 가져오기
     getResponse(() => {
-      var ratings = rating%1;
-      ratings = (ratings.toFixed(1))*10;
+      var ratings = rating % 1;
+      ratings = ratings.toFixed(1) * 10;
       rating = Math.floor(rating);
 
       res.render("mapstival/detail", {
@@ -212,9 +105,9 @@ router.get("/detail", async function (req, res, next) {
         data: tourData,
         detail: detail_Data,
         rating: rating,
-        ratings:ratings,
+        ratings: ratings,
         ratingPutNumber: ratingPutNumber,
-        dbData: dbData,
+        value: value,
       });
     });
   } catch (err) {
@@ -222,6 +115,7 @@ router.get("/detail", async function (req, res, next) {
   }
 });
 
+//구글 평점 리뷰 API
 function getResponse(callback) {
   var reviewName = encodeURI(gtitle); //리뷰 검색 키워드
   var reviewlat = gmapx; // 리뷰 적도
@@ -229,7 +123,7 @@ function getResponse(callback) {
 
   axios
     .get(
-      `https://maps.googleapis.com/maps/api/place/search/json?location=${reviewequ},${reviewlat}&radius=500&types=point_of_interest&name=${reviewName}&key=${GAPI_KEY}`
+      `https://maps.googleapis.com/maps/api/place/search/json?location=${reviewequ},${reviewlat}&radius=500&types=point_of_interest&name=${reviewName}&key=${GOOGLE_KEY}`
     )
     .then((response) => {
       if (response.data.status == "OK") {
@@ -237,12 +131,9 @@ function getResponse(callback) {
 
         axios
           .get(
-            `https://maps.googleapis.com/maps/api/place/details/json?reference=${reference}&key=${GAPI_KEY}`
+            `https://maps.googleapis.com/maps/api/place/details/json?reference=${reference}&key=${GOOGLE_KEY}`
           )
           .then((response) => {
-            // for (i = 0; i < 10; i++) {
-            //   tourData.push(response.data.response.body.items.item[i]);
-            // }
             rating = response.data.result.rating;
             ratingPutNumber = response.data.result.user_ratings_total;
 
@@ -257,15 +148,23 @@ function getResponse(callback) {
       }
     });
 }
-// 지도페이지 이동
+
+// 구글맵
 const lat = [];
 const equ = [];
 const id = [];
 const gmapTitle = [];
 const gmapUrl = [];
-router.get("/gmap", function (req, res, next) {
-  axios
-    .get(`${URL}`)
+//구글맵 이동
+router.get("/gmap", async function (req, res, next) {
+  const gmapData = {
+    area: req.query.area,
+    date: req.query.date,
+  };
+  await axios
+    .get(
+      `${FESTIVAL_URL}searchFestival?serviceKey=${FESTIVAL_KEY}${FESTIVAL_ETC}&listYN=Y&areaCode=${gmapData.area}&pageNo=1&numOfRows=50&eventEndDate=${gmapData.date}`
+    )
     .then((response) => {
       let tourData = [];
       const list = response.data.response.body.items.item;
@@ -284,8 +183,8 @@ router.get("/gmap", function (req, res, next) {
       }
       //위도 경도 넣는곳
       for (i = 0; i < tourData.length; i++) {
-        lat.push(tourData[i].mapy);
-        equ.push(tourData[i].mapx);
+        lat.push(parseFloat(tourData[i].mapy));
+        equ.push(parseFloat(tourData[i].mapx));
         gmapTitle.push(tourData[i].title);
         gmapUrl.push(tourData[i].mapx);
         id.push(tourData[i].contentid);
@@ -293,10 +192,11 @@ router.get("/gmap", function (req, res, next) {
       res.render("mapstival/gmap", {
         lat: lat,
         equ: equ,
-        f_area: f_area,
+        f_area: gmapData.area,
         gmapTitle: gmapTitle,
         tourData: tourData,
         id: id,
+        API: GOOGLE_KEY,
       });
       lat.length = 0;
       equ.length = 0;
@@ -304,14 +204,9 @@ router.get("/gmap", function (req, res, next) {
       id.length = 0;
     })
     .catch((e) => {
+      console.log(e);
       res.send(e);
     });
 });
-
-// 행사 날짜 설정
-function apiSetting(startDate, f_area, FNumber) {
-  URL = `${API_URL}searchFestival?serviceKey=${API_KEY}${API_ETC}&listYN=Y&areaCode=${f_area}&pageNo=1&numOfRows=${FNumber}&eventEndDate=${startDate}`;
-  return URL;
-}
 
 module.exports = router;
